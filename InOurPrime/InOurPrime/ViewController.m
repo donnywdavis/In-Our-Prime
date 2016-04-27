77cdfd8e9c9058774e259782873d27bd29373c12
Donny Davis <donnywdavis@icloud.com>
2016-04-27 15:21:21 -0400

    Set up picker to select a calculation option

--- InOurPrime/InOurPrime/ViewController.m
+++ InOurPrime/InOurPrime/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "PrimeBrain.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *instructionsLabel;
 @property (weak, nonatomic) IBOutlet UITextField *numbersTextField;
@@ -16,13 +17,25 @@
 @property (weak, nonatomic) IBOutlet UILabel *answerLabel;
 @property (weak, nonatomic) IBOutlet UIPickerView *calculationPicker;
 
+@property (strong, nonatomic) NSArray *calculationOptionsArray;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Set the delegate and the data source for the picker
+    self.calculationPicker.delegate = self;
+    self.calculationPicker.dataSource = self;
+    // Set our available calculation options for the picker
+    self.calculationOptionsArray = @[@"", @"Check Prime Number", @"Get all prime factors", @"Get largest common prime factor"];
+    // Disable the check button until a calculation option is selected
+    self.checkAnswerButton.enabled = NO;
+    self.instructionsLabel.text = @"";
+    self.answerLabel.text = @"";
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,6 +46,45 @@
 #pragma mark - Button Actions
 
 - (IBAction)checkAnswer:(UIButton *)sender {
+}
+
+#pragma mark - UIPickerViewDelegate
+
+- (nullable NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return self.calculationOptionsArray[row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    switch (row) {
+        case 1:
+            self.instructionsLabel.text = @"Enter a number to check if it is a prime number or not.";
+            break;
+            
+        case 2:
+            self.instructionsLabel.text = @"Enter a number to see all of the prime factors for that number";
+            break;
+            
+        case 3:
+            self.instructionsLabel.text = @"Enter two numbers separated by a comma to see the largest common prime number between them.";
+            break;
+            
+        default:
+            self.instructionsLabel.text = @"";
+            self.checkAnswerButton.enabled = NO;
+            break;
+    }
+}
+
+#pragma mark - UIPickerViewDataSource
+
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.calculationOptionsArray.count;
 }
 
 @end
